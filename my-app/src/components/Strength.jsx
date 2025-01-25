eeacce5a7997ed75097f7fc7beaa5b1d50c952e2
Arthur Kenji <arthurkenji.47@gmail.com>
2025-01-24 23:25:59 -0300

    Update Strength.jsx
    Apenas um teste para a força da senha

--- my-app/src/components/Strength.jsx
+++ my-app/src/components/Strength.jsx
@@ -1,12 +1,41 @@
 //Indica a força da senha gerada
-
 import React from 'react';
-
 const Strength = ({ password }) => {
     const getStrength = (password) => {
-        //IMPLEMENTAR LOGICA DE AVALIAR SENHA
-    };
 
+        let IntStrength = 0;
+
+        if (options.hasUpper){
+            IntStrength += 6;
+        }
+        if (options.hasLower){
+            IntStrength += 6;
+        }
+        if (options.hasNumber){
+            IntStrength += 3;
+        }
+        if (options.hasSymbols){
+            IntStrength += 3;
+        }
+
+        IntStrength += PWlength;
+
+        if (IntStrength >= ){
+            return 'Strong';
+        }
+
+        else if (IntStrength > ){
+            return 'MEDIUM';
+        }
+
+        else if (IntStrength > ){
+            return 'WEAK';
+        }
+
+        else {
+            return 'TOO WEAK!'
+        }
+    };
     return (
         <div>
             <p>Strength: {getStrength(password)}</p>
