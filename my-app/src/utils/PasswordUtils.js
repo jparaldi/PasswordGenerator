23cc3a930290826150a2c48ed8d43d0a52134cdc
Arthur <arthurkenji.47@gmail.com>
2025-01-24 22:48:20 -0300

    Update PasswordUtils.js

--- my-app/src/utils/PasswordUtils.js
+++ my-app/src/utils/PasswordUtils.js
@@ -1,22 +1,21 @@
 export const generatePassword = ({length, hasUpper, hasLower, hasNumber, hasSymbols}) => {
-  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const lower = 'abcdefghijklmnopqrstuvwxyz';
-  const numbers = '0123456789';   
-  const symbols = '!@#$%^&*()_+{}:"<>?|[];\',./';
-  let all = '';
-  let password = '';
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lower = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';   
+    const symbols = '!@#$%^&*()_+{}:"<>?|[];\',./';
+    let all = '';
 
-  if (hasUpper) all += upper;
-  if (hasLower) all += lower;
-  if (hasNumber) all += numbers;
-  if (hasSymbols) all += symbols;
+    if(hasUpper) all += upper;
+    if(hasLower) all += lower;
+    if(hasNumber) all += numbers;
+    if(hasSymbols) all += symbols;
 
-  if (!all) return "";
+    if(!all)return "";
 
-  for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * all.length);
-      password += all[randomIndex];
-  }
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+      }
 
-  return password;
+    return password;
 };
