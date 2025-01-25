cd2d4868f4ebcdae2043b256d3bde78ad2c43114
Arthur Kenji <191265991+Ar7Kenji@users.noreply.github.com>
2025-01-24 21:09:15 -0300

    Update PasswordArea.jsx
    Acredito que está função para o Botão de copy esteja já funcionando.

--- my-app/src/components/PasswordArea.jsx
+++ my-app/src/components/PasswordArea.jsx
@@ -1,18 +1,29 @@
-//Mostra a senha Gerada
 //Botão de copiar a senha
-
-
 import React from 'react';
-
 const PasswordArea = ({ password }) => {
-    console.log(password);
+    const handleCopyPassword = () => {
+        if (password) {
+            navigator.clipboard.writeText(password).then(() => {
+                alert('PASSWORD COPIED');
+            })
+            .catch((err) =>{
+                alert ('ERROR WHEN COPYING PASSWORD');
+                console.error(err);
+            });
+        }
+        else {
+            alert ('NO PASSWORD TO COPY');
+        }
+    };
+
     return (
         <div>
             <input type="text" value={password} readOnly placeholder="Your Password will be here" />
+            <button onClick={handleCopyPassword} style={{background: 'none', border : 'none', cursor: 'pointer'}}>
+                <img src='aqui vai ficar o caminho pro ícone do Copy' alt='CopyButton'/>
+            </button>
         </div>
     );
 };
-
-//IMPLEMENTAR O BOTÃO DE COPIAR A SENHA
 
 export default PasswordArea;
